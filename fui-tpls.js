d8bda2a51bf85259c99f280d67c2c5129eb013b4
Kouta Imanaka <keima@p-side.net>
2015-05-25 21:15:15 +0900

    Append dependencies

---
+++ fui-tpls.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name fui
+ * @description
+ *
+ * This creates our fui Components module
+ * add all needed directives to this fui components module
+ *
+ */
+
+angular.module('fui',['fui.template','fui.checkbox','fui.switch','fui.radio']);
